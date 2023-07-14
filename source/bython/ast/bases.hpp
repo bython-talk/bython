7b18273033cab799e4b9269afb5a5c38294edc04
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-07-14 17:44:46 +0200

    Helper functions for recognising and downcasting

--- source/bython/ast/bases.hpp
+++ source/bython/ast/bases.hpp
@@ -1,10 +1,46 @@
 #pragma once
 
+#include <memory>
+
+namespace bython::visitation
+{
+struct visitor;
+struct transformer;
+}  // namespace bython::visitation
+
 namespace bython::ast
 {
 struct node
 {
   virtual ~node() = default;
+
+  virtual auto visit(visitation::visitor& visitor) const -> void = 0;
+  /*virtual auto transform(visitation::transformer& transformer) const
+      -> std::unique_ptr<node> = 0;*/
 };
+
+template<typename T>
+auto isa(node const* ast) -> bool
+{
+  return dynamic_cast<T*>(ast) != nullptr;
+}
+
+template<typename T>
+auto isa(node* ast) -> bool
+{
+  return dynamic_cast<T*>(ast) != nullptr;
+}
+
+template<typename T>
+auto dyn_cast(node const* ast) -> T const*
+{
+  return dynamic_cast<T const*>(ast);
+}
+
+template<typename T>
+auto dyn_cast(node* ast) -> T*
+{
+  return dynamic_cast<T*>(ast);
+}
 
 }  // namespace bython::ast
