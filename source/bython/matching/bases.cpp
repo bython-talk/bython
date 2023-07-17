00d722758904544cca9ba25969f91680163b3962
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-07-17 15:29:15 +0200

    Remove unused function

--- source/bython/matching/bases.cpp
+++ source/bython/matching/bases.cpp
@@ -5,12 +5,6 @@
 namespace bython::matching
 {
 
-auto matches(std::unique_ptr<ast::node> const& ast,
-             std::unique_ptr<matcher> const& matcher) -> bool
-{
-  return matches(*ast, *matcher);
-}
-
 auto matches(ast::node const& ast, matcher const& matcher) -> bool
 {
   return matcher.matches(ast);
