eefaabc8315c2709b7c97019af7945dd9a560171
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-07-18 20:16:57 +0200

    Parse branching as a statement

--- source/bython/parser/grammar.hpp
+++ source/bython/parser/grammar.hpp
@@ -22,13 +22,23 @@ static constexpr auto funcdef_ = LEXY_KEYWORD("def", internal::identifier);
 static constexpr auto return_ = LEXY_KEYWORD("return", internal::identifier);
 static constexpr auto variable_ = LEXY_KEYWORD("val", internal::identifier);
 static constexpr auto struct_ = LEXY_KEYWORD("struct", internal::identifier);
+
+static constexpr auto if_ = LEXY_KEYWORD("if", internal::identifier);
+static constexpr auto elif_ = LEXY_KEYWORD("elif", internal::identifier);
+static constexpr auto else_ = LEXY_KEYWORD("else", internal::identifier);
 }  // namespace keyword
 
 namespace internal
 {
-static constexpr auto reserved_identifier = identifier.reserve(
-    keyword::funcdef_, keyword::return_, keyword::variable_, keyword::struct_);
-}
+static constexpr auto reserved_identifier =
+    identifier.reserve(keyword::funcdef_,
+                       keyword::return_,
+                       keyword::variable_,
+                       keyword::struct_,
+                       keyword::if_,
+                       keyword::elif_,
+                       keyword::else_);
+}  // namespace internal
 
 struct symbol_identifier
 {
@@ -266,32 +276,101 @@ struct expression
   static constexpr auto value = lexy::forward<std::unique_ptr<ast::expression>>;
 };
 
+/* === Statements === */
+template<typename T>
+static constexpr auto new_statement =
+    grammar::new_unique_ptr<T, ast::statement>;
+
+template<typename T>
+static constexpr auto new_compound_statement =
+    grammar::new_unique_ptr<T, ast::compound>;
+
 /* === Inner Statements === */
+
+struct branch_body
+{
+  static constexpr auto rule = dsl::recurse<struct inner_compound_body>;
+  static constexpr auto value = lexy::forward<ast::statements>;
+};
+
+struct dependent_branch
+{
+  static constexpr auto rule = []
+  {
+    auto another_elif = dsl::p<nested_expression> + dsl::p<branch_body>
+        + dsl::opt(dsl::recurse_branch<struct dependent_branch>);
+    auto terminating_else = dsl::p<branch_body>;
+
+    return keyword::elif_ >> another_elif | keyword::else_ >> terminating_else;
+  }();
+
+  static constexpr auto value =
+      lexy::callback(lexy::construct<ast::conditional_branch>
+                         | new_compound_statement<ast::conditional_branch>,
+                     lexy::construct<ast::unconditional_branch>
+                         | new_compound_statement<ast::unconditional_branch>);
+};
+
+struct conditional_branch
+{
+  static constexpr auto rule = []
+  {
+    auto alt_condition = dsl::p<dependent_branch>;
+
+    auto introduced = dsl::p<nested_expression> + dsl::p<branch_body>
+        + dsl::opt(alt_condition);
+    return keyword::if_ >> introduced;
+  }();
+
+  static constexpr auto value =
+      lexy::bind(lexy::construct<ast::conditional_branch>,
+                 lexy::_1,
+                 lexy::_2,
+                 lexy::_3.or_default())
+      | new_compound_statement<ast::conditional_branch>;
+};
 
 struct assignment
 {
   static constexpr auto rule = []
   {
-    auto introduced = dsl::whitespace(dsl::ascii::space)
-        + dsl::p<grammar::symbol_identifier>
-        + LEXY_LIT("=") + dsl::p<expression>;
+    auto introduced =
+        dsl::p<grammar::symbol_identifier> + LEXY_LIT("=") + dsl::p<expression>;
     return keyword::variable_ >> introduced;
   }();
 
-  static constexpr auto value = lexy::construct<ast::assignment>
-      | lexy::new_<ast::assignment, std::unique_ptr<ast::statement>>;
+  static constexpr auto value =
+      lexy::construct<ast::assignment> | new_statement<ast::assignment>;
 };
 
 struct inner_stmt
 {
-  static constexpr auto rule = [] { return dsl::p<assignment>; }();
+  struct missing_statement
+  {
+    static constexpr auto name =
+        R"(Expected `val` for an assignment, or `if` for branching;
+  This error can also occur if you forgot to finish branching with a closing curly bracket)";
+  };
+
+  static constexpr auto rule = []
+  {
+    auto terminator = dsl::terminator(dsl::semicolon).limit(dsl::lit_c<'}'>);
+    return terminator(dsl::p<assignment> | dsl::p<conditional_branch>
+                      | dsl::error<missing_statement>);
+  }();
 
   static constexpr auto value = lexy::forward<std::unique_ptr<ast::statement>>;
 };
 
-/* === Outer Statements === */
+struct inner_compound_body
+{
+  static constexpr auto rule = []
+  { return dsl::curly_bracketed.opt_list(dsl::recurse<struct inner_stmt>); }();
 
-struct compound_body
+  static constexpr auto value = lexy::as_list<ast::statements>;
+};
+
+struct outer_compound_body
 {
   static constexpr auto rule = []
   {
@@ -301,6 +380,8 @@ struct compound_body
 
   static constexpr auto value = lexy::as_list<ast::statements>;
 };
+
+/* === Outer Statements === */
 
 struct function_def
 {
@@ -324,7 +405,7 @@ struct function_def
   static constexpr auto rule = []
   {
     auto introduced = dsl::p<grammar::symbol_identifier> + dsl::p<parameters>
-        + dsl::p<compound_body>;
+        + dsl::p<outer_compound_body>;
     return keyword::funcdef_ >> introduced;
   }();
 
@@ -380,7 +461,7 @@ struct mod
   static constexpr auto rule = []
   {
     // top-level statements are separated by newline
-    return dsl::terminator(dsl::newline).list(dsl::p<outer_stmt>);
+    return dsl::list(dsl::p<outer_stmt>, dsl::sep(dsl::newline));
   }();
 
   static constexpr auto value =
