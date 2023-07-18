5643a7b8c6f7d3397525e5ee4cdbc18e7988d230
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-07-18 20:16:27 +0200

    Add tests for if statements

--- test/parser/inner_stmt.cpp
+++ test/parser/inner_stmt.cpp
@@ -7,11 +7,72 @@
 
 namespace ast = bython::ast;
 namespace m = bython::matching;
+namespace grammar = bython::grammar;
 
 TEST_CASE("Assignment")
 {
-  auto ast =
-      unwrap_grammar<bython::grammar::inner_stmt, ast::assignment>("val x = f");
-  REQUIRE(m::matches(
-      ast, m::assignment {std::string {"x"}, m::lift<m::variable>("f")}));
+  auto ast = unwrap_grammar<grammar::inner_stmt, ast::assignment>("val x = f;");
+  auto matcher = m::lift<m::assignment>("x", m::lift<m::variable>("f"));
+  REQUIRE(m::matches(ast, *matcher));
+}
+
+TEST_CASE("If Statement")
+{
+  SECTION("Empty If")
+  {
+    auto ast = unwrap_grammar<grammar::inner_stmt, ast::conditional_branch>(
+        "if x { };");
+  }
+
+  SECTION("Single If")
+  {
+    auto ast = unwrap_grammar<grammar::inner_stmt, ast::conditional_branch>(
+        "if x { val y = x; val z = y; };");
+  }
+
+  SECTION("If + Elif")
+  {
+    auto ast = unwrap_grammar<grammar::inner_stmt, ast::conditional_branch>(R"(
+if x {
+  val y = 2;
+} elif a {
+  val y = 3;
+};
+)");
+  }
+
+  SECTION("If + Elif + Elif")
+  {
+    auto ast = unwrap_grammar<grammar::inner_stmt, ast::conditional_branch>(R"(
+if x {
+  val y = 2;
+} elif a {
+  val y = 3;
+} elif b {
+  val y = 4;
+};
+)");
+  }
+
+  SECTION("If + Else")
+  {
+    auto ast = unwrap_grammar<grammar::inner_stmt, ast::conditional_branch>(R"(
+if x {
+  val y = 2;
+} else {
+  val y = 3;
+};
+)");
+  }
+
+  SECTION("Missing Semicolon")
+  {
+    unwrap_grammar_failure<grammar::inner_stmt, ast::conditional_branch>(R"(
+if x {
+  val y = 2;
+} elif z {
+  val y = 3;
+}
+)");
+  }
 }
