2d0e827ebcfd201fe0ceb13150279a5f1da18b90
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-08-02 19:40:20 +0200

    Config LIT binary

--- test/codegen/lit.cfg.py
+++ test/codegen/lit.cfg.py
@@ -3,4 +3,11 @@ import lit.formats
 config.name = "codegen"
 config.test_format = lit.formats.ShTest(True)
 
-config.suffixes = ['.by']
+config.suffixes = [".by"]
+
+config.test_source_root = os.path.dirname(__file__)
+config.test_exec_root = os.path.join(config.binary_dir, "test", "lit-output")
+
+config.substitutions.append(
+    (r"%jit", os.path.join(config.binary_dir, r"bython-jit"))
+)
