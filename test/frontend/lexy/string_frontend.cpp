90d8a37a95541cbdcb1db6b7d0d35b4376e065a2
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-09-05 11:12:50 +0000

    Show how to label separate parts of the AST

--- test/frontend/lexy/string_frontend.cpp
+++ test/frontend/lexy/string_frontend.cpp
@@ -1,5 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include "bython/ast.hpp"
 #include "bython/frontend/lexy.hpp"
 
 namespace p = bython::parser;
@@ -10,13 +11,28 @@ using node_ptr = std::unique_ptr<ast::node>;
 TEST_CASE("Lexy Frontend")
 {
   auto parser = p::lexy_code_frontend {};
-  auto parse_result = parser.parse("undecl_iden");
+  auto parse_result = parser.parse("undecl_iden,DeclIdent");
   REQUIRE(parse_result.has_value());
 
-  auto [parse_tree, ast] = std::move(parse_result).value();
-  parser.report_error(*parse_tree,
-                      *ast,
+  auto [parse_metadata, ast] = std::move(parse_result).value();
+  auto mod = dynamic_cast<ast::expr_mod*>(&*ast);
+  REQUIRE(mod != nullptr);
+
+  parser.report_error(*parse_metadata,
+                      *mod->body[0],
                       p::frontend_error_reporter {}
                           .message("undeclared identifier found; did you mean: undeclIdent")
+                          .build());
+
+  parser.report_error(*parse_metadata,
+                      *mod->body[1],
+                      p::frontend_error_reporter {}
+                          .message("undeclared identifier found; did you mean: DeclIdentifier")
+                          .build());
+
+  parser.report_error(*parse_metadata,
+                      *mod,
+                      p::frontend_error_reporter {}
+                          .message("Unknown notation; Please clarify with parentheses")
                           .build());
 }
